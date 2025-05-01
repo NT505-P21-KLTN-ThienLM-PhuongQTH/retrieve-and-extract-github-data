8b14987bb4b7dcd3ae4796ba53248a1a5096ec54
th1enlm02 <21521460@gm.uit.edu.vn>
2025-05-02 01:56:36 +0700

    remove useless stages

--- lib/ghtorrent/ghtorrent.rb
+++ lib/ghtorrent/ghtorrent.rb
@@ -57,8 +57,10 @@ module GHTorrent
     end
 
     def stages
+      # %w(ensure_commits ensure_topics ensure_languages ensure_pull_requests
+      #    ensure_issues ensure_watchers ensure_labels ensure_forks ensure_workflows)
       %w(ensure_commits ensure_topics ensure_languages ensure_pull_requests
-         ensure_issues ensure_watchers ensure_labels ensure_forks ensure_workflows)
+         ensure_issues ensure_labels ensure_workflows)
     end
 
     def ensure_workflows(owner, repo)
