59a239a35f48a09cca4e641b13c5bf5be9d3a7ec
th1enlm02 <21521460@gm.uit.edu.vn>
2025-06-12 14:53:58 +0700

    minor fixes

--- lib/ghtorrent/commands/full_repo_retriever.rb
+++ lib/ghtorrent/commands/full_repo_retriever.rb
@@ -10,8 +10,10 @@ module GHTorrent
       include GHTorrent::EventProcessing
 
       def stages
+        # %w(ensure_commits ensure_topics ensure_languages ensure_pull_requests
+        #   ensure_issues ensure_watchers ensure_labels ensure_forks ensure_workflows)
         %w(ensure_commits ensure_topics ensure_languages ensure_pull_requests
-          ensure_issues ensure_watchers ensure_labels ensure_forks ensure_workflows)
+         ensure_issues ensure_labels ensure_workflows)
       end
 
       def settings
